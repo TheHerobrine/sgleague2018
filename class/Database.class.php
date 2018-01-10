afb1aa68d9c2b887832534f66637e98fc222770a
Victor Rouquette <victor@rouquette.me>
2018-01-11 00:52:50 +0100

    Database class modified

--- class/Database.class.php
+++ class/Database.class.php
@@ -2,14 +2,14 @@
 class Database
 {
 	private $database;
-	
+
 	public function __construct()
 	{
 		$hote = "localhost";
 		$db = "sgleague";
 		$login = "sgleague";
 		$pass = ""; // Z'avez cru quoi ?
-		
+
 		try
 		{
 			$this->database = new PDO('mysql:host='.$hote.';dbname='.$db.';charset=UTF8', $login, $pass);
@@ -20,15 +20,15 @@ class Database
 		}
 	}
 
-	public function req($req)
+	public function req_get($req)
 	{
-		$return = $this->database->query($req) or die(print_r($this->database->errorInfo()));
-		return $return;
+		return $this->database->query($req);
 	}
 
-	public function close_sql()
+	public function req_post($req, array $params)
 	{
-		$this->database->closeCursor();
+		$resp = $this->database->prepare($req);
+		return $resp->execute($params);
 	}
 }
 ?>
