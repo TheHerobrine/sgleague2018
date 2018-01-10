b8cdbf1ca3fe27e37cbf21b7aaebe0ce6f050507
Victor Rouquette <victor@rouquette.me>
2018-01-10 22:46:49 +0100

    CONNECT_USER query added

--- sql/queries_select.sql
+++ sql/queries_select.sql
@@ -24,12 +24,27 @@ DELIMITER |
 -- Insert procedure
 
 -- Other
--- TODO: CONNECT_USER
+DROP PROCEDURE IF EXISTS CONNECT_USER;
 
 -- ************************************************************************************************ --
 -- ----------------------------- Create Queries --------------------------------------------------- --
 -- ************************************************************************************************ --
 
 -- -------------------------------------------------------------------------------------------------------------------------------------------- --
--- INSERT_SGL_USER
+-- CONNECT_USER
+-- Error:1 <= Bad login or password
 -- ---
+
+CREATE PROCEDURE CONNECT_USER( IN login_or_mail INTEGER, IN pass VARCHAR(512), IN config_salt VARCHAR(512))
+BEGIN
+  DECLARE id_user_confirmed INTEGER DEFAULT NULL;
+
+  SELECT SU_UID INTO id_user_confirmed FROM T_SGL_USER
+    WHERE (SU_LOGIN=login_or_mail OR SU_MAIL=login_or_mail) AND (SU_PASS=SHA1(CONCAT(CONCAT(SU_SALT,pass),config_salt)));
+
+  IF id_user_confirmed IS NOT NULL THEN
+    SELECT TRUE as RESULT, SU_UID,SU_ID_PARENT_SU,SU_ID_CARD_F,SU_ID_S,SU_LOGIN,SU_MAIL,SU_TYPE,SU_GENDER,SU_BIRTH_DATE,SU_FIRST_NAME,SU_LAST_NAME FROM T_SGL_USER WHERE SU_UID=id_user_confirmed;
+  ELSE
+    SELECT FALSE as RESULT, 1 as ERROR;
+  END IF;
+END |
