e5e8a7bc7415cf795de828829533b2c50245d8b3
Victor Rouquette <victor@rouquette.me>
2018-01-11 22:52:47 +0100

    File class partially completed

--- class/File.class.php
+++ class/File.class.php
@@ -23,6 +23,11 @@ class File
 	/**
 	 * @var string
 	 */
+	protected $key;
+
+	/**
+	 * @var string
+	 */
 	protected $url;
 
 	/**
@@ -54,7 +59,12 @@ class File
 	/**
 	 * @var bool
 	 */
-	protected $init;
+	protected $init_get;
+
+	/**
+	 * @var bool
+	 */
+	protected $init_post;
 
 	/**
 	 * File constructor.
@@ -68,25 +78,37 @@ class File
 	/**
 	 * Reset file information
 	 */
-	public function reset_file() {
+	public function reset_file()
+	{
 		$this->init = false;
 	}
 
-	public function init_for_get($id) {
+	public function init_for_get($id)
+	{
 
-		if($this->init == true) {
+		if($this->init_post)
+		{
+			return false;
+		}
+
+		if($this->init_get)
+		{
 			return true;
 		}
 
 		$req = $this->database->req_get('CALL SELECT_FILE('.$id.');');
-		if($data = $req->fetch()){
+		$data = $req->fetch();
+		$req->closeCursor();
+
+		if($data)
+		{
 			$this->id = $data['F_UID'];
 			$this->name = $data['F_NAME'];
 			$this->path = $data['F_PATH'];
 			$this->url = $data['F_PATH'];
 			$this->size = $data['F_SIZE'];
 			$this->type = $data['F_TYPE'];
-			$this->init = true;
+			$this->init_get = true;
 			return true;
 		}
 
@@ -94,34 +116,81 @@ class File
 		return false;
 	}
 
-	public function init_for_post($key) {
+	public function init_for_post($key, $path)
+	{
+		if($this->init_get) {
+			return false;
+		}
 
-		if($this->init ==true) {
+		if($this->init_post)
+		{
 			return true;
-		} else {
+		}
+
+		else
+		{
 			//TODO: content
 			return false;
 		}
 
 	}
 
-	public function get_url() {
+	public function get_url()
+	{
+		if($this->init_get)
+		{
+			return $this->url;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public function get_info()
+	{
+		if($this->init_get)
+		{
+			return array(
+				'STATUS' => 'GET',
+				'ID' => $this->id,
+				'URL' => $this->url,
+				'PATH' => $this->path,
+				'NAME' => $this->name,
+				'TYPE' => $this->type,
+				'SIZE' => $this->size,
+				'MD5' => $this->md5
+			);
+		}
+		else if($this->init_post)
+		{
+			return array(
+				'STATUS' => 'POST',
+				'NAME' => $_FILES[$this->key]['name'],
+				'TMP_PATH' => $_FILES[$this->key]['tmp_name'],
+				'TYPE' => $_FILES[$this->key]['type'],
+				'SIZE' => $_FILES[$this->key]['size'],
+				'MD5' => md5_file($_FILES[$this->key]['tmp_name'])
+			);
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public function post()
+	{
 
 	}
 
-	public function get_info() {
+	public function update()
+	{
 
 	}
 
-	public function post() {
-
-	}
-
-	public function update() {
-
-	}
-
-	public function file_exist() {
+	public function file_exist()
+	{
 
 	}
 
@@ -135,7 +204,8 @@ class File
 
 	}
 
-	public function delete() {
+	public function delete()
+	{
 
 	}
 }
